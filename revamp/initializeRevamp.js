3cca9cfd6ae51c05408e1030568a127dd6b1e42f
Touyet <touyette29@gmail.com>
2020-05-16 19:02:57 +0200

    start of revamp

---
+++ revamp/initializeRevamp.js
@@ -0,0 +1,94 @@
+const itemsImgData = {
+	// Single Items
+	["bomb"]: { default: "./normal/bombbag.png", tagnia: "", soli: "" },
+	["boomerang"]: { default: "./normal/boomerang.png", tagnia: "", soli: "" },
+	["bow"]: { default: "./normal/bow.png", tagnia: "", soli: "" },
+	["hammer"]: { default: "./normal/hammer.png", tagnia: "", soli: "" },
+	["irons"]: { default: "./normal/ironboots.png", tagnia: "", soli: "" },
+	["slingshot"]: { default: "./normal/slingshot.png", tagnia: "", soli: "" },
+	["bombchu"]: { default: "./normal/bombchu.png", tagnia: "", soli: "" },
+	["lens"]: { default: "./normal/lens.png", tagnia: "", soli: "" },
+	["magic"]: { default: "./normal/magic.png", tagnia: "", soli: "" },
+	["hovers"]: { default: "./normal/hoversboots.png", tagnia: "", soli: "" },
+	["mirror"]: { default: "./normal/mirrorshield.png", tagnia: "", soli: "" },
+
+	// Bottles
+	["letter"]: { default: "./normal/letter.png", tagnia: "", soli: "" },
+	["bottle"]: { default: "./normal/bottle.png", tagnia: "", soli: "" },
+
+	// Hookshots
+	["hookshot1"]: { default: "./normal/hookshot.png", tagnia: "", soli: "" },
+	["hookshot2"]: { default: "./normal/longshot.png", tagnia: "", soli: "" },
+
+	// Strength
+	["str1"]: { default: "./normal/strength1.png", tagnia: "", soli: "" },
+	["str2"]: { default: "./normal/strength2.png", tagnia: "", soli: "" },
+	["str3"]: { default: "./normal/strength3.png", tagnia: "", soli: "" },
+
+	// Scales
+	["scale1"]: { default: "./normal/silverscale.png", tagnia: "", soli: "" },
+	["scale2"]: { default: "./normal/goldenscale.png", tagnia: "", soli: "" },
+
+	// Fairy spell
+	["farores"]: { default: "./normal/farores.png", tagnia: "", soli: "" },
+	["dins"]: { default: "./normal/dinsfire.png", tagnia: "", soli: "" },
+
+	// Elemental Arrows
+	["firearrows"]: { default: "./normal/firearrows.png", tagnia: "", soli: "" },
+	["lightarrows"]: { default: "./normal/lightarrows.png", tagnia: "", soli: "" },
+
+	// Child Songs
+	["lullaby"]: { default: "./normal/lullaby.png", tagnia: "", soli: "" },
+	["epona"]: { default: "./normal/eponas.png", tagnia: "", soli: "" },
+	["saria"]: { default: "./normal/sarias.png", tagnia: "", soli: "" },
+	["sun"]: { default: "./normal/suns.png", tagnia: "", soli: "" },
+	["time"]: { default: "./normal/sot.png", tagnia: "", soli: "" },
+	["storms"]: { default: "./normal/sos.png", tagnia: "", soli: "" },
+
+	// Adult Songs
+	["prelude"]: { default: "./normal/prelude.png", tagnia: "", soli: "" },
+	["minuet"]: { default: "./normal/minuet.png", tagnia: "", soli: "" },
+	["bolero"]: { default: "./normal/bolero.png", tagnia: "", soli: "" },
+	["serenade"]: { default: "./normal/serenade.png", tagnia: "", soli: "" },
+	["nocturne"]: { default: "./normal/nocturne.png", tagnia: "", soli: "" },
+	["requiem"]: { default: "./normal/requiem.png", tagnia: "", soli: "" }
+}
+
+const locationImgData = {
+
+	// Fairies
+	["dins"]: { default: "./normal/dinsfairy.png", tagnia: "", soli: "" },
+	["defense"]: { default: "./normal/gfairy.png", tagnia: "", soli: "" },
+
+	// Cutscene
+	["lacs"]: { default: "./normal/la.png", tagnia: "", soli: "" },
+
+	// Dungeons
+	["deku"]: { default: "./normal/deku.png", tagnia: "", soli: "" },
+	["dodongo"]: { default: "./normal/revamp/dodongo.png", tagnia: "", soli: "" },
+	["jabu"]: { default: "./normal/jabu.jpg", tagnia: "", soli: "" },
+	["ice"]: { default: "./normal/ice.png", tagnia: "", soli: "" },
+
+	// Towns
+	["forest"]: { default: "./normal/kokiri.png", tagnia: "", soli: "" },
+	["ranch"]: { default: "./normal/cow.png", tagnia: "", soli: "" },
+	["market"]: { default: "./normal/market.png", tagnia: "", soli: "" },
+	["kak"]: { default: "./normal/kakariko.png", tagnia: "", soli: "" },
+	["graveyard"]: { default: "./normal/graveyard.png", tagnia: "", soli: "" },
+	["goron"]: { default: "./normal/goron.png", tagnia: "", soli: "" },
+	["domain"]: { default: "./normal/revamp/domain.png", tagnia: "", soli: "" },
+	["fortress"]: { default: "./normal/fortress.jpg", tagnia: "", soli: "" },
+
+	// Overworld
+	["dmt"]: { default: "./normal/revamp/dmt.png", tagnia: "", soli: "" },
+	["crater"]: { default: "./normal/crater.png", tagnia: "", soli: "" },
+	["fountain"]: { default: "./normal/fountain.png", tagnia: "", soli: "" },
+	["lostwoods"]: { default: "./normal/lostWoods.png", tagnia: "", soli: "" },
+	["sfm"]: { default: "./normal/sfm.png", tagnia: "", soli: "" },
+	["river"]: { default: "./normal/river.jpg", tagnia: "", soli: "" },
+	["colossus"]: { default: "./normal/DesertColossus2.jpg", tagnia: "", soli: "" },
+	["wasteland"]: { default: "./normal/Haunted_Wasteland.jpg", tagnia: "", soli: "" },
+	["hyrule"]: { default: "./normal/revamp/hyruleField.png", tagnia: "", soli: "" },
+	["valley"]: { default: "./normal/Gerudo_Valley.png", tagnia: "", soli: "" },
+	["lake"]: { default: "./normal/revamp/lake.png", tagnia: "", soli: "" },
+}
